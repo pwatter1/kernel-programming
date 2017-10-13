c6c8ea275744783dda7a3c0c5992f088891c587f
root <root@CS350-pwatter1.csvb.local>
2017-10-12 22:49:32 -0400

    final commit to test userside xtime()

--- xtime/test_xtime.c
+++ xtime/test_xtime.c
@@ -8,12 +8,13 @@ int main(void)
 {
 	struct timespec ts;
 	int retVal;
-	retVal = syscall(326, &ts);
+
+	retVal = syscall(326, &ts);	
 
 	printf("Function return value: %d \nErrno number: %d \nTime: %ld\n", 
 		retVal, 
 		errno, 
 		ts.tv_sec*1000000000UL + ts.tv_nsec);
-	
+
 	return 0;
 }

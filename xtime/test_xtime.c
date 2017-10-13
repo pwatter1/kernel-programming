eda6038d67beeb8333318665428cebfc8b890717
root <root@CS350-pwatter1.csvb.local>
2017-10-12 22:29:22 -0400

    error checking user interface

--- xtime/test_xtime.c
+++ xtime/test_xtime.c
@@ -2,19 +2,17 @@
 #include <time.h>
 #include <unistd.h>
 #include <linux/unistd.h>
+#include <errno.h>
 
-
-
-int main(void) {
-
+int main(void) 
+{
 	struct timespec ts;
-	int ret;
+	int retVal;
 
-	while(1)
-	{
-		ret = syscall(326, &ts);
-		printf("ret=%d time= %ld\n", ret, ts.tv_sec*1000000000UL + ts.tv_nsec);
-		sleep(1);
-	}
+	retVal = syscall(326, &ts);
+	printf("Function return value: %d \n
+		Errno number: %d \n
+		Time: %ld\n", 
+		retVal, errno, ts.tv_sec*1000000000UL + ts.tv_nsec);
 	return 0;
 }
